fc75535cd46e821055853cc75711080c436e3801
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-05-18 17:29:54 -0400

    fix import to make antd work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Timer from './Components/Timer.js';
 import History from './Components/History.js';
 import Records from './Components/Records.js';
 import TimerControls from './Components/TimerControls.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="dummyHeader">Intermittent Fasting Tracker!</h1>
-      <div className="grid">
-        <TimerControls />
-        <Timer />
-        <History />
-        <Records />
-        {/* <div className="timerControls">timerControls</div>
-        <div className="timer">timer</div>
-        <div className="history">history</div>
-        <div className="records">records</div> */}
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      fasts: [
+        {
+          startDate: 10000,
+          endDate: 10001,
+          actualTime: 0,
+          plannedTime: 0,
+          passed: true
+        }
+      ],
+      timerTime: 0,
+      timerStart: 0,
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="dummyHeader">Intermittent Fasting Tracker!</h1>
+        <div className="grid">
+          <TimerControls />
+          <Timer />
+          <History />
+          <Records />
+        </div>
+
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
--- src/Components/History.js
+++ src/Components/History.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './History.css';
+import { isEmpty } from "lodash";
 
 class History extends Component {
 
     render() {
         return (
             <div className="history">
-                History
+                <div className="sectionHeader">History</div>
+
+                {isEmpty(this.props.fasts) &&
+                    <div className="emptyMessage">
+                        No fasts yet. Select a template fast or create a custom fast to start!
+                </div>
+                }
+
+                {!isEmpty(this.props.fasts) &&
+
+                    <div className="historyBody">
+
+                    </div>
+
+                }
             </div>
         );
     }
