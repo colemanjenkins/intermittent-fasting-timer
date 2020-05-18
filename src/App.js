cedef685fc46480270b1677c61e278944a5aa5b2
acao <alc3pf@virginia.edu>
2020-05-18 16:59:55 -0400

    starting timer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Timer from './Components/Timer.js';
 import History from './Components/History.js';
 import Records from './Components/Records.js';
 import TimerControls from './Components/TimerControls.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="dummyHeader">Intermittent Fasting Tracker!</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      fasts: [
+        {
+          startDate: 10000,
+          endDate: 10001,
+          actualTime: 0,
+          plannedTime: 0,
+          passed: true
+        }
+      ],
+      timerTime: 0,
+      timerStart: 0,
+
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1 className="dummyHeader">Intermittent Fasting Tracker!</h1>
+        <Timer/><br/><br/>
+        <History
+          fasts = {this.state.fasts}
+        />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/Components/History.js
+++ src/Components/History.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './History.css';
+import { isEmpty } from "lodash";
 
 class History extends React.Component {
 
     render() {
         return(
-            <div>
+            <div className="history">
+                <div className="sectionHeader">History</div>
 
+                {isEmpty(this.props.fasts) &&
+                <div className="emptyMessage">
+                    No fasts yet. Select a template fast or create a custom fast to start!
+                </div>
+                }
+
+                {!isEmpty(this.props.fasts) &&
+
+                <div className="historyBody">
+
+                </div>
+                
+                }
             </div>
         );
     }
