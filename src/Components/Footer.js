fa9a8881f03e83b51c82318c06cad52f4e45d395
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-05-21 13:50:29 -0400

    remove background and fix footer alignment

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,50 +9,43 @@ class Footer extends Component {
                 <MDBFooter color="blue" className="font-small pt-4 mt-4">
                     <MDBContainer fluid className="text-center text-md-left">
                         <MDBRow>
-                        <MDBCol md="6">
-                            <h5 className="title">From the devs:</h5>
-                            <p>
-                            Thank you for using Intermittent Fasting Timer!
+                            <MDBCol md="6">
+                                <h5 className="title">From the devs:</h5>
+                                <p>
+                                    Thank you for using Intermittent Fasting Timer!
                             </p>
-                        </MDBCol>
-                        <MDBCol md="6">
-                            <h5 className="title">Additional Resources</h5>
-                            <ul classname="links">
-                                <li className="list-unstyled">
-                                    <a target="_blank" href="https://www.healthline.com/nutrition/intermittent-fasting-guide">
+                            </MDBCol>
+                            <MDBCol md="6">
+                                <h5 className="title">Additional Resources</h5>
+                                <a target="_blank" href="https://www.healthline.com/nutrition/intermittent-fasting-guide">
                                     Intermittent Fasting 101 — The Ultimate Beginner's Guide
-                                    </a>
-                                </li>
-                                <li className="list-unstyled">
-                                    <a target="_blank" href="https://www.healthline.com/nutrition/6-ways-to-do-intermittent-fasting">
+                                </a>
+                                <div>{" "}</div>
+                                <a target="_blank" href="https://www.healthline.com/nutrition/6-ways-to-do-intermittent-fasting">
                                     6 Popular Ways to Do Intermittent Fasting
-                                    </a>
-                                </li>
-                                <li className="list-unstyled">
-                                    <a target="_blank" href="https://www.health.harvard.edu/blog/intermittent-fasting-surprising-update-2018062914156">
+                                </a>
+                                <div>{" "}</div>
+                                <a target="_blank" href="https://www.health.harvard.edu/blog/intermittent-fasting-surprising-update-2018062914156">
                                     Harvard Health Blog Article
-                                    </a>                                
-                                </li>
-                                <li className="list-unstyled">
-                                    <a target="_blank" href="https://www.webmd.com/diet/news/20191226/intermittent-fasting-diet-could-boost-your-health#1">
+                                </a>
+                                <div>{" "}</div>
+                                <a target="_blank" href="https://www.webmd.com/diet/news/20191226/intermittent-fasting-diet-could-boost-your-health#1">
                                     Intermittent Fasting Health Impacts
-                                    </a>
-                                </li>
-                                <li className="list-unstyled">
-                                    <a target="_blank" href="https://www.medicalnewstoday.com/articles/327398">
+                                </a>
+                                <div>{" "}</div>
+                                <a target="_blank" href="https://www.medicalnewstoday.com/articles/327398">
                                     Intermittent Fasting Tips
-                                    </a>
-                                </li>
-                            </ul>
-                        </MDBCol>
+                                </a>
+                                <br></br>
+                            </MDBCol>
                         </MDBRow>
                     </MDBContainer>
                     <div className="footer-copyright text-center py-3">
                         <MDBContainer fluid>
-                        &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+
                         </MDBContainer>
                     </div>
-                    </MDBFooter>
+                </MDBFooter>
             </div>
         );
     }
