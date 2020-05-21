b6faed57266c12bbc109929859225fda2720449f
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-05-21 14:51:46 -0400

    fix history cards

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,23 +17,23 @@ class Footer extends Component {
                             </MDBCol>
                             <MDBCol md="6">
                                 <h5 className="title">Additional Resources</h5>
-                                <a target="_blank" href="https://www.healthline.com/nutrition/intermittent-fasting-guide">
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.healthline.com/nutrition/intermittent-fasting-guide">
                                     Intermittent Fasting 101 — The Ultimate Beginner's Guide
                                 </a>
                                 <div>{" "}</div>
-                                <a target="_blank" href="https://www.healthline.com/nutrition/6-ways-to-do-intermittent-fasting">
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.healthline.com/nutrition/6-ways-to-do-intermittent-fasting">
                                     6 Popular Ways to Do Intermittent Fasting
                                 </a>
                                 <div>{" "}</div>
-                                <a target="_blank" href="https://www.health.harvard.edu/blog/intermittent-fasting-surprising-update-2018062914156">
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.health.harvard.edu/blog/intermittent-fasting-surprising-update-2018062914156">
                                     Harvard Health Blog Article
                                 </a>
                                 <div>{" "}</div>
-                                <a target="_blank" href="https://www.webmd.com/diet/news/20191226/intermittent-fasting-diet-could-boost-your-health#1">
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.webmd.com/diet/news/20191226/intermittent-fasting-diet-could-boost-your-health#1">
                                     Intermittent Fasting Health Impacts
                                 </a>
                                 <div>{" "}</div>
-                                <a target="_blank" href="https://www.medicalnewstoday.com/articles/327398">
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.medicalnewstoday.com/articles/327398">
                                     Intermittent Fasting Tips
                                 </a>
                                 <br></br>
